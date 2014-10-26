0d8953c783c90b1816f8a190d69f9f61a007da80
fmouaki <fmouaki@users.noreply.github.com>
2014-10-26 10:55:50 -0400

    Update Run_analysis.R

--- Run_analysis.R
+++ Run_analysis.R
@@ -1,50 +1,50 @@
 Install.packages("reshape")
 library(reshape)
-library(plyr))
+library(plyr)
 
-xTrain = read.table("./train/X_train.txt")
-yTrain = read.table("./train/y_train.txt")
-subjectTrain = read.table("./train/subject_train.txt")
+xTrain<-read.table("./train/X_train.txt")
+yTrain<-read.table("./train/y_train.txt")
+subjectTrain<-read.table("./train/subject_train.txt")
 
-xTest = read.table("./test/X_test.txt")
-yTest = read.table("./test/y_test.txt")
-subjectTest = read.table("./test/subject_test.txt")
+xTest<-read.table("./test/X_test.txt")
+yTest<-read.table("./test/y_test.txt")
+subjectTest<-read.table("./test/subject_test.txt")
 
-featuresdf = read.table("./features.txt")
-headings = featuresdf$V2
+featuresdf<-read.table("./features.txt")
+headings<-featuresdf$V2
 
 #transfer headings to data set
-colnames(xTrain) = headings
-colnames(xTest) = headings
+colnames(xTrain)<-headings
+colnames(xTest)<-headings
 
 #change V1 variable to descriptive variable "activity"
-yTest <- rename(yTest, c(V1="activity"))
-yTrain <- rename(yTrain, c(V1="activity"))
+yTest<-rename(yTest, c(V1="activity"))
+yTrain<-rename(yTrain, c(V1="activity"))
 
 # change data values in yTest according to activity_labels.txt file
-activitydf  = read.table("./activity_labels.txt")
+activitydf<-read.table("./activity_labels.txt")
 
 #convert variable names to lowercase
-activityLabels = tolower(levels(activitydf$V2))
+activityLabels<-tolower(levels(activitydf$V2))
 
 #convert $activity to factor and add descriptive labels
-yTrain$activity = factor(yTrain$activity,labels = activityLabels)
-yTest$activity = factor(yTest$activity,yTest$activity,labels = activityLabels)
+yTrain$activity<-factor(yTrain$activity,labels = activityLabels)
+yTest$activity<-factor(yTest$activity,labels = activityLabels)
 
-subjectTrain <- rename(subjectTrain, c(V1="subjectid"))
-subjectTest <- rename(subjectTest, c(V1="subjectid"))
+subjectTrain<-rename(subjectTrain, c(V1="subjectid"))
+subjectTest<-rename(subjectTest, c(V1="subjectid"))
 
-train = cbind(xTrain, subjectTrain, yTrain)
-test = cbind(xTest, subjectTest, yTest)
+train<-cbind(xTrain, subjectTrain, yTrain)
+test<-cbind(xTest, subjectTest, yTest)
 
 #combine data set
-finalData = rbind(train, test)
+finalData<-rbind(train, test)
 
-pattern = "mean|std|subjectid|activity"
-tidyData = finalData[,grep(pattern , names(finalData), value=TRUE)]
+pattern<-"mean|std|subjectid|activity"
+tidyData<-finalData[,grep(pattern , names(finalData), value=TRUE)]
 
 # summarize data
-result = ddply(tidyData, .(activity, subjectid), numcolwise(mean))
+result<-ddply(tidyData, .(activity, subjectid), numcolwise(mean))
 
 # write file to output
 write.table(result, file="result.txt", sep = "\t", append=F)
